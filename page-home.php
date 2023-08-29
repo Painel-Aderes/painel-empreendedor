0f3d85640c1553e8f594708dd69adfb6876507f9
Isabela Oliveira <oliver.isabela2303@gmail.com>
2023-08-28 22:43:38 -0300

    feat: index - perfil section

--- page-home.php
+++ page-home.php
@@ -9,3 +9,58 @@
     </div>
   </div>
 </main>
+
+<section class="index-section">
+  <div class="container">
+    <img class="index-section-image__desktop" src="" alt="">
+
+    <div class="index-section-info">
+      <h2>Microcrédito personalizado para você</h2>
+
+      <p class="texto-lg">O suporte financeiro ideal para impulsionar seu negócio com o microcrédito personalizado do Painel do Empreendedor.</p>
+
+      <a href="" class="btn-primario btn-index-after --cor-primaria">Acessar microcrédito</a>
+    </div>
+  </div>
+</section>
+
+<section class="section-perfil">
+  <div class="container">
+    <h2>Acesse dados estatísticos personalizados para o seu perfil</h2>
+
+    <ul class="perfil-lista">
+      <li class="perfil-item">
+        <img class="perfil-item-image" src="https://picsum.photos/344/280?grayscale" alt="">
+
+        <div class="perfil-item-info">
+          <h3>Perfil do Empreendedor</h3>
+          <p class="texto-sm">Descubra informações detalhadas e relevantes sobre o cenário empreendedor do Espírito Santo.</p>
+
+          <a href="" class="btn-primario btn-index-after --cor-primaria">Acessar perfil</a>
+        </div>
+      </li>
+
+      <li class="perfil-item">
+        <img class="perfil-item-image" src="https://picsum.photos/344/280?grayscale" alt="">
+
+        <div class="perfil-item-info">
+          <h3>Perfil do Artesão</h3>
+          <p class="texto-sm">Obtenha dados valiosos sobre o mercado de artesanato capixaba.</p>
+
+          <a href="" class="btn-primario btn-index-after --cor-primaria">Acessar perfil</a>
+        </div>
+      </li>
+
+      <li class="perfil-item">
+        <img class="perfil-item-image" src="https://picsum.photos/344/280?grayscale" alt="">
+
+        <div class="perfil-item-info">
+          <h3>Perfil do Catador</h3>
+          <p class="texto-sm">Descubra como o trabalho sustentável dos catadores contribui para o crescimento econômico.</p>
+
+          <a href="" class="btn-primario btn-index-after --cor-primaria">Acessar perfil</a>
+        </div>
+      </li>
+    </ul>
+  </div>
+</section>
